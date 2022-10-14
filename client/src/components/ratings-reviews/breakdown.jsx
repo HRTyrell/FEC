0882c6834fce830174c2617c002dfcd047f5acf9
Ben Tanaka <BenT07891@gmail.com>
2022-10-14 13:25:49 -0500

    added calculating avg rating

--- client/src/components/ratings-reviews/breakdown.jsx
+++ client/src/components/ratings-reviews/breakdown.jsx
@@ -1,10 +1,16 @@
 import {useState, useEffect} from 'react';
 
-const Breakdown = ({metaData, setstarBarFilters}) {
+const Breakdown = function ({metaData, setstarBarFilters}) {
 
   console.log(metaData);
 
-  let averageRating =
+  let averageRating = [];
+  for (var key in metaData.ratings) {
+    averageRating = averageRating.concat(new Array(Number(metaData.ratings[key])).fill(Number(key)));
+  }
+  averageRating = averageRating.reduce((accumulator, val)=>{return accumulator + val}, 0) / averageRating.length;
+  console.log(averageRating)
+
   return (
     <div>RATINGS & REVIEWS
 
