4844e52794697893f1399006aac3317bf4b66d6c
Drew Henderson <drewsworld16@gmail.com>
2022-10-17 10:23:03 -0500

    Began work on Carousel for RelatedItems

---
+++ client/src/components/RelatedItems/Carousel.jsx
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const CarouselStyled = styled.div`
+
+`;
+
+const Carousel = () => {
+
+  return (
+    <CarouselStyled>
+
+    </CarouselStyled>
+  )
+}
+
+export default CarouselStyled;
--- client/src/components/RelatedItems/ProductCard.jsx
+++ client/src/components/RelatedItems/ProductCard.jsx
@@ -7,27 +7,34 @@ border-style: solid;
 border-spacing: 20px;
 width: 100px;
 height: 200px;
-padding: 5px;
+padding: 0;
 `;
+
+// const ProductImageOuterDiv = styled.div`
+// width: 100px;
+// height: 66.6666px;
+// `;
 
 const ProductImageStyled = styled.div`
+display: flex;
+align-items: center;
+margin: auto;
 position: relative;
 width: 100px;
-height: 133.3333
-height:
+height: 133.3333px;
+
 `;
 
-const ProductDescription = styled.div`
-height: 66.6666px;
-`;
 
 const ImageStyled = styled.img`
+display: flex;
+align-items: center
 position: relative;
 display: block;
 margin-left: auto;
 margin-right: auto;
 width: 100px;
-height: 133.333px;
+height: auto;
 padding: 5px;
 padding-top: 25px;
 `;
@@ -37,9 +44,15 @@ position: absolute;
 top 10px;
 right: 0px;
 flex: none;
-width: 30px;
+width: 15px;
 `;
 
+const OuterDescriptionDiv = styled.div`
+width: 105px;
+height: 72.35px;
+padding: 5px;
+background-color: rgba(211, 211, 211, 0.5);
+`;
 const Category = styled.p`
 margin-top: 0;
 margin-bottom: 0;
@@ -86,14 +99,15 @@ const ProductCard = ({product}) => {
         <ImageStyled src={image} alt="Image of RelatedProduct" />
         <ButtonStyled src="./star-empty-icon.png"
           onMouseEnter={handleOnMouseEnter}
-          onMouseLeave={handleOnMouseLeave}/>
+          onMouseLeave={handleOnMouseLeave}
+          aria-label="Comparison Button"/>
       </ProductImageStyled>
-      <div >
+      <OuterDescriptionDiv>
         <Category>{product.category}</Category>
         <ProductTitle>{product.name}</ProductTitle>
         <Price>{product.default_price}</Price>
         <Review src={handleReview()} />
-      </div>
+      </OuterDescriptionDiv>
     </CardStyled>
   )
 }
