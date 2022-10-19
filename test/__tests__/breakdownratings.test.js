84fa8677f220d226573b042a0a372b41741a1342
Ben Tanaka <BenT07891@gmail.com>
2022-10-19 12:32:44 -0500

    refactored to use '@testing-library/react' instead of '@testing-library/dom'

--- test/__tests__/breakdownratings.test.js
+++ test/__tests__/breakdownratings.test.js
@@ -3,27 +3,12 @@
  * @jest-environment jsdom
  */
 
-import React from "react";
-import {render, unmountComponentAtNode } from "react-dom";
-import {act } from "react-dom/test-utils";
-import {getByLabelText, getByText, getByRole} from '@testing-library/dom';
+import {render, screen, cleanup} from '@testing-library/react';
 
 const {Breakdown} = require("../../client/src/components/ratings-reviews/breakdown.jsx");
 const {ProductBreakdownFactor} = require("../../client/src/components/ratings-reviews/breakdown.jsx");
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+afterEach(()=>{cleanup()});
 
 let fakeData = {
   "product_id": "66642",
@@ -59,14 +44,7 @@ let fakeData = {
 }
 const FakestarBarFilters = {1:true,2:true,3:true,4:true,5:true,filtered:false}
 
-const mockChildComponent = jest.fn();
-jest.mock(ProductBreakdownFactor, () => (props) => {
-  mockChildComponent(props);
-  return <mock-childComponent />;
-});
-
-describe("breakdown", () => {
-
+describe('breakdown', ()=> {
   test("it should render an average rating of 3.6", () => {
     fakeData.ratings = {
       "1": "0",
@@ -75,31 +53,27 @@ describe("breakdown", () => {
       "4": "3",
       "5": "1"
     }
-    render(<Breakdown metaData={fakeData} starBarFilters={FakestarBarFilters} setstarBarFilters={()=>{}}/>, container);
-
-    expect(getByRole(container, 'averagerating').textContent).toEqual("3.6");
-  });
+    render(<Breakdown metaData={fakeData} starBarFilters={FakestarBarFilters} setstarBarFilters={()=>{}}/>);
+    const actualAvgRating = screen.getByTestId('averagerating').textContent;
+    expect(actualAvgRating).toEqual('3.6');
+  })
 
   test("it should render a label of '39 total reviews'", () => {
     fakeData.recommended = {
       "false": 8,
       "true": 31
     }
-
-    render(<Breakdown metaData={fakeData} starBarFilters={FakestarBarFilters} setstarBarFilters={()=>{}}/>, container);
-
-    expect(getByText(container, '39 total reviews')).not.toBeNull()
-  });
+    render(<Breakdown metaData={fakeData} starBarFilters={FakestarBarFilters} setstarBarFilters={()=>{}}/>);
+    expect(screen.getByText('39 total reviews')).not.toBeNull();
+  })
 
   test("it should render a label of '45% of reviews recommend this product'", () => {
     fakeData.recommended = {
       "false": 55,
       "true": 45
     }
-
-    render(<Breakdown metaData={fakeData} starBarFilters={FakestarBarFilters} setstarBarFilters={()=>{}}/>, container);
-
-    expect(getByText(container, '45% of reviews recommend this product')).not.toBeNull()
-  });
+    render(<Breakdown metaData={fakeData} starBarFilters={FakestarBarFilters} setstarBarFilters={()=>{}}/>);
+    expect(screen.getByText('45% of reviews recommend this product')).not.toBeNull();
+  })
 
 });
