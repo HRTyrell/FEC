74b70228e9ef1dade3fd6127d97d1eb2cc0be337
Gabriela Olarte <gmolarte@gmail.com>
2022-10-19 16:40:23 -0500

    add styling

--- client/src/components/Q&A/Answers.jsx
+++ client/src/components/Q&A/Answers.jsx
@@ -3,19 +3,29 @@ import axios from "axios";
 import styled from "styled-components";
 import {TOKEN} from "/MyConfig.js";
 
+const SoftButton = styled.button`
+  border: none;
+  background:none;
+  text-decoration: underline;
+  color: #404040;
+`
+
+const A = styled.div`
+  display: flex-column;
+  padding: 10px;
+  // border: dotted;
+  // border-color: green;
+  height: relative;
+  width: 100%;
+`
 
 const Answers= ({question}) => {
   const [answers, setAnswers] = useState([]);
   const [id, setId] = useState(question.question_id);
   const [view, setView] = useState(2);
-  const [viewAll, setViewAll] = useState(false);
-
-
-
-
+  const [allAnswers, setAllAnswers] = useState(false);
 
   useEffect( () => {
-
     axios({
       method: 'get',
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/qa/questions/${question.question_id}/answers`,
@@ -29,20 +39,18 @@ const Answers= ({question}) => {
     .catch((err) => (console.log('GET ANSWER', err)))
   }, [id])
 
-
-
   return (
     <div>
       {answers.slice(0,2).map((answer) => (
-        <>
-          <label></label>
-          <p>A:    {answer.body}</p>
+        <A>
+          <p><b>A: &nbsp; </b>  {answer.body}</p>
           <footer>
-            <small>{answer.answerer_name},  {answer.date} | Helpful? <softButton >Yes</softButton >({answer.helpfulness}) | <softButton >Report</softButton ></small>
+            <small> &nbsp; &nbsp; by {answer.answerer_name},  {answer.date} | Helpful? <SoftButton >Yes</SoftButton >({answer.helpfulness}) |
+            <SoftButton >Report</SoftButton ></small>
           </footer>
-        </>
+        </A>
       ))}
-           {(answers.length > 2 && viewAll === false)  ? <> <button onClick={(e) => setViewAll(true) }>LOAD MORE ANSWERS</button></> : viewAll === true ?  <button onClick={(e) => setViewAll(False) }>HIDE ANSWERS</button> : <></> }
+           {(answers.length > 2 && allAnswers === false)  ? <> <SoftButton onClick={(e) => setAllAnswers(true) }>LOAD MORE ANSWERS</SoftButton></> : allAnswers === true ?  <button onClick={(e) => setAllAnswers(False) }>HIDE ANSWERS</button> : <></> }
     </div>
   )
 }
