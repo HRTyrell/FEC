d2e7d1ef356dc5245697f396e12ffbc57f4f2b60
Ben Tanaka <BenT07891@gmail.com>
2022-10-19 19:00:50 -0500

    completed business docs section 1.2.1 and 1.2.2 for review list and individual review tile

--- client/src/components/ratings-reviews/reviewTile.jsx
+++ client/src/components/ratings-reviews/reviewTile.jsx
@@ -1,8 +1,10 @@
 import {useState, useEffect} from 'react';
 import styled from "styled-components";
-import {Starbar} from './starbar.jsx'
+import {Starbar} from './starbar.jsx';
+import axios from 'axios';
+import {TOKEN} from '/MyConfig.js';
 
-const StyledContainer = styled.div`
+const StyledContainerZ = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -11,10 +13,26 @@ const StyledContainer = styled.div`
 const StyledBold = styled.header`
   font-weight: bold;
 `
-
+const StyledModal = styled.header`
+  position: fixed;
+  z-index: 100;
+  padding: 200px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: black;
+  background-color: rgba(0,0,0,0.8); /* Fallback color */
+`
+const StyledOuterContainer = styled.div`
+  padding: 10px 0px;
+  border-top: 2px solid grey;
+`
 export const ReviewTile = ({review}) => {
 
   const [showMore, setShowMore] = useState(review.body.length > 250 ? true : false)
+  const [helpful, setHelpful] = useState({voted:false, helpfulCount: review.helpfulness})
 
   const convertDate = (inp)=> {
     let formattedDate = new Date(inp)
@@ -22,12 +40,28 @@ export const ReviewTile = ({review}) => {
     return `${formattedDate[1]} ${formattedDate[2]}, ${formattedDate[3]}`
   }
 
+  const uploadHelpfulVote = ()=> {
+    if (!helpful.voted) {
+      axios({
+        url: `http://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/reviews/${review.review_id}/helpful`,
+        method: 'put',
+        headers: {authorization: TOKEN}
+        })
+        .then(()=> {
+          setHelpful({voted:true, helpfulCount: review.helpfulness + 1})
+        })
+        .catch((err)=> {
+          alert(err);
+        })
+    }
+  }
+
   return (
-    <div>
-      <StyledContainer>
+    <StyledOuterContainer>
+      <StyledContainerZ>
         <Starbar rating={review.rating}/>
-        <time>{convertDate(review.date)}</time>
-      </StyledContainer>
+        <time>{review.reviewer_name + ', ' + convertDate(review.date)}</time>
+      </StyledContainerZ>
       <StyledBold>{review.summary}</StyledBold>
 
       {showMore ?
@@ -39,13 +73,30 @@ export const ReviewTile = ({review}) => {
       <div>{review.body}</div>
       }
 
-      <StyledContainer>
+      <StyledContainerZ>
         {review.photos.map((photo, index)=> {
-          return <img key={index} src={photo.url} width="80px" height="80px"></img>
+          return <SizeAdjustableImage key={index} url={photo.url}/>
         })}
-
-      </StyledContainer>
-    </div>
+      </StyledContainerZ>
+      {review.recommend && <div>✓ I recommend this product</div>}
+      {review.response && <div>RESPONSE FROM SELLER: {review.response}</div>}
+      <b>Was this review helpful? </b>
+      <a href="#/" onClick={uploadHelpfulVote}>Yes ({helpful.helpfulCount})</a>
+    </StyledOuterContainer>
 
   )
+}
+
+const SizeAdjustableImage = ({url}) => {
+  const [modalView, setModalView] = useState(false)
+
+  if (modalView) {
+    return (
+      <StyledModal>
+        <img src={url} onClick={()=>{setModalView(false)}}></img>
+      </StyledModal>
+    )
+  } else {
+    return <img src={url} width="70px" height="70px" onClick={()=>{setModalView(true)}}></img>
+  }
 }
