a024ec8b47ea9eae21b5dbf4204d1149d88ca851
balaji <balajisathiya12@gmail.com>
2022-10-21 09:22:12 -0500

    Halfway through Gallery component

--- client/src/components/ProductDetail/StyleSelector.jsx
+++ client/src/components/ProductDetail/StyleSelector.jsx
@@ -4,33 +4,61 @@ import styled from "styled-components";
 import ProductStore from "../Provider/Zus_Provider.jsx";
 
 const ImgS = styled.img`
-height: 50px;
-width: 50px;
+height: 80px;
+width: 80px;
 border-radius: 50%;
 border: solid;
 `
+const ImgY = styled.img`
+height: 80px;
+width: 80px;
+border-radius: 50%;
+border: solid;
+border-color: red;
+`
 
 const DivS = styled.div`
+width: 100%;
 display: flex;
 flex-direction: row;
-gap: 5px;
+flex-wrap: wrap;
+gap: 10px;
+
+justify-content: center;
 `
+
+//Completion of State for Styles
+//TODO Completion of choice on Style
+//TODO CSS of Selector
+//TODO Passing Style state to parent
 
 const StyleSelector = () => {
 
-  const Styles = ProductStore((state) => state.curProductStyles)
+  const Styles = ProductStore((state) => state.curProductStyles);
+  const curStyle = ProductStore((state) => state.curStyle);
+
+  const setStyle = ProductStore((state) => state.setStyle);
 
   if (!Styles) {
     return null;
   }
 
+  const handleClick = (e) => {
+    setStyle(e.target.title);
+  }
+
+  console.log(curStyle)
+
   return (
     <DivS>
       {Styles.map((info) => {
+        if (info.style_id === curStyle.style_id) {
+          return(
+            <ImgY title = {info.name} key={info.style_id} value={info.style_id} src={info.photos[0].thumbnail_url} height="30px" width="30px"/>
+          )
+        }
         return(
-          <>
-            <ImgS title = {info.name} key={info.style_id} src={info.photos[0].thumbnail_url} height="30px" width="30px"/>
-          </>
+          <ImgS title = {info.name} key={info.style_id} value={info.style_id} src={info.photos[0].thumbnail_url} height="30px" width="30px" onClick = {(e) => handleClick(e)} />
         )
       })}
     </DivS>
